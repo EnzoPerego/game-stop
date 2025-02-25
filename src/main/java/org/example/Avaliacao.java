fe2267bad63cbfe7f19546ea833013c5cf0e402e
Raphaellafer <raphaelcl@al.insper.edu.br>
2025-02-25 16:43:55 -0300

    .

--- src/main/java/org/example/Avaliacao.java
+++ src/main/java/org/example/Avaliacao.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Avaliacao {
+    private int nota;
+    private Usuario usuario;
+
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
