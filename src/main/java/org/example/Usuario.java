30674f5b1b30138617ee7354215b7cc7bfa19876
kauamakiyama <kauabc@al.insper.edu.br>
2025-02-25 16:36:59 -0300

    usuario feito

--- src/main/java/org/example/Usuario.java
+++ src/main/java/org/example/Usuario.java
@@ -1,4 +1,42 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Usuario {
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String email;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
